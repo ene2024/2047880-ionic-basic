34fa8f5c2dfe8ee7dcf60a8c6d64b43f9824145e
Valeria-YL <valeria.ybarral@uanl.edu.mx>
2024-02-20 21:15:39 -0600

    practica3

---
+++ script.js
@@ -0,0 +1,10 @@
+function login(){
+    var usr=document.getElementById("usr");
+    var psswrd=document.getElementById("psswrd");
+    alert('login realizado con exito'+ usr.value);
+}
+
+function registro(){
+    var usr=document.getElementById("usr");
+    alert('Registro exitoso'+usr.value);
+}
