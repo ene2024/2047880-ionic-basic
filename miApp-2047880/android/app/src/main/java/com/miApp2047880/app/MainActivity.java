afc3fc962cc1ba4180661a16d5eb2e2924dc83c6
Valeria-YL <valeria.ybarral@uanl.edu.mx>
2024-05-21 16:25:15 -0600

    practica 13

---
+++ miApp-2047880/android/app/src/main/java/com/miApp2047880/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.miApp2047880.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- miApp-2047880/capacitor.config.ts
+++ miApp-2047880/capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.2047880.app',
   appName: 'miApp-2047880',
   webDir: 'www',
   server: {
